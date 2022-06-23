6bc1fb4faf01fbc095097317d94651bc9d53234a
Lucas Ribeiro <lucaribeiro.dev@gmail.com>
2022-06-23 00:27:12 -0300

    concluindo a primeira fase, rodando liso

--- dados.py
+++ dados.py
@@ -4,17 +4,18 @@ import sys
 print("Dados Disponíveis: D3 [3], D4 [4], D6 [6], D8 [8], D10 [10], D12 [12], D20 [20], D100 [100]")
 repeat_rolling = True
 while repeat_rolling:
-    dado = int(input("Escolha a quantidade de lados do seu dado: "))
+    dado1 = int(input("Escolha a quantidade de lados do seu dado: "))
     quantidade = int(input('Quantos dados quer jogar? '))
 
     lista_dados = [3,4,6,8,10,12,20,100]
-    num = randint(1,dado+1)
+    num = randint(1,dado1)
+    resultado = num*quantidade
 
     for dado in lista_dados:
-        if (dado == dado):
-            print(f'Você rolou o seguinte número usando os dados: {num}')
+        if (dado1 == dado):
+            print(f'A soma dos seus dados ({quantidade}) jogados é: {resultado}')
             print('Você quer jogar novamente(Y/N)?')
-repeat_rolling = ("y" or "yes") in input().lower()
+            repeat_rolling = ("y" or "yes") in input().lower()
 
 
 
